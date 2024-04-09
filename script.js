8194b35667dbf7b0e036df2ee3442975742e2fe8
Soumetsiuu <161454781+Soumetsiuu@users.noreply.github.com>
2024-04-09 09:04:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,7 @@ function searchFunction() {
     filter = input.value.toUpperCase();
     ul = document.getElementById("searchResults");
     li = ul.getElementsByTagName('li');
-    const cards = document.querySelectorAll('.card');
-
-    cards.forEach(card => {
-    card.addEventListener('click', () => {
-    // Logic to show a modal or fullscreen image here
-    // (You might use libraries like Lightbox.js)
-    // Optionally, transition to the next card using logic here
-  });
-});
-
-   const cardContainer = document.querySelector('.card-container');
-   const cards = cardContainer.querySelectorAll('.card');
-
-cards.forEach(card => {
-  card.addEventListener('click', () => {
-    cards.forEach(otherCard => otherCard.classList.remove('active')); // Deactivate other cards
-    card.classList.add('active'); // Activate clicked card
-  });
-});
-    
+  
     for (i = 0; i < li.length; i++) {
       a = li[i].getElementsByTagName("a")[0];
       txtValue = a.textContent || a.innerText;
